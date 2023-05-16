631f1d624f3796a8fa1a58e57d0167aaaf440d6b
Ronak <ronakbhawnani2@gmail.com>
2023-05-17 01:25:21 +0530

    Lab Files Added

---
+++ LE1_Prob1.java
@@ -0,0 +1,10 @@
+public class Main
+{
+	public static void main(String[] args) {
+	int a = Integer.parseInt(args[0]);
+	for(int i=0; i<=10; i++)
+	{
+	    System.out.println(a+"x"+i+"="+a*i);
+	}
+	}
+}
---
+++ LE1_Prob3.java
@@ -0,0 +1,21 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+	   int i,j,k;
+	   for(i=1; i<=4; i++)
+	   {
+	       for (j=3; j>=i; j--)
+	       {
+	          System.out.print(" ");
+	       }
+	       for(k=1; k<=i; k++)
+	        {
+	           System.out.print("* ");
+	        }
+	       System.out.println();
+	   }
+    }
+
+}
+
